8a98d5b9b5a6babb0671fed70b4e14db1d525aa4
taiwogbadamosi <124234997+taiwogbadamosi@users.noreply.github.com>
2024-05-17 17:31:01 +0100

    hi

---
+++ script.js
@@ -0,0 +1,10 @@
+const share_button = document.getElementById("share");
+const nav = document.getElementById("off")
+//have an event listener on share button
+share_button.addEventListener("click", () => {
+    nav.style.display = "flex";
+    
+
+} )
+// when it is triggered
+// bring out new nav
